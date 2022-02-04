ac9b8203fe45d1023f465b6804da8900b072765b
Gerrylon <gerrylon@163.com>
2022-02-04 15:57:42 +0800

    update memory-limit

--- test/cgroups/memory-limit.go
+++ test/cgroups/memory-limit.go
@@ -22,9 +22,8 @@ func main() {
 		cmd.Stdin = os.Stdin
 		cmd.Stdout = os.Stdout
 		cmd.Stderr = os.Stderr
-		if err := cmd.Run(); err != nil {
-			panic(err)
-		}
+		err := cmd.Run()
+		checkErr(err, "/proc/self/exe run")
 	}
 	cmd := exec.Command("/proc/self/exe")
 	cmd.SysProcAttr = &syscall.SysProcAttr{
@@ -32,18 +31,26 @@ func main() {
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-	if err := cmd.Start(); err != nil {
-		panic(err)
-	} else {
-		// 得到 fork 出来进程映射在外部命名空间的 pid
-		fmt.Printf("%v\n", cmd.Process.Pid)
-		// 在系统默认创建挂载了 memory subsystem Hierarchy 上创建 cgroup
-		os.Mkdir(path.Join(cgroupMemoryHierarchyMount, "testmemorylimit"), 0755)
-		// 将容器进程加入到这个 cgroup
-		ioutil.WriteFile(path.Join(cgroupMemoryHierarchyMount, "testmemorylimit", "tasks"), []byte(strconv.Itoa(cmd.Process.Pid)), 0644)
-		//g限制 cgroup 进程使用
-		ioutil.WriteFile(path.Join(cgroupMemoryHierarchyMount, "testmemorylimit", "memory.limit_in_bytes"), []byte("100m"), 0644)
+	err := cmd.Start()
+	checkErr(err, "/proc/self/exe start")
+	// 得到 fork 出来进程映射在外部命名空间的 pid
+	fmt.Printf("%v\n", cmd.Process.Pid)
+	// 在系统默认创建挂载了 memory subsystem Hierarchy 上创建 cgroup
+	err = os.Mkdir(path.Join(cgroupMemoryHierarchyMount, "testmemorylimit"), 0755)
+	checkErr(err, "Mkdir")
+	// 将容器进程加入到这个 cgroup
+	err = ioutil.WriteFile(path.Join(cgroupMemoryHierarchyMount, "testmemorylimit", "tasks"), []byte(strconv.Itoa(cmd.Process.Pid)), 0644)
+	checkErr(err, "WriteFile tasks")
+	//g限制 cgroup 进程使用
+	err = ioutil.WriteFile(path.Join(cgroupMemoryHierarchyMount, "testmemorylimit", "memory.limit_in_bytes"), []byte("100m"), 0644)
+	checkErr(err, "WriteFile limit_in_bytes")
 
-		cmd.Process.Wait()
+	_, err = cmd.Process.Wait()
+	checkErr(err, "cmd.Process.Wait")
+}
+
+func checkErr(err error, reason string) {
+	if err != nil {
+		panic(fmt.Sprintf("err:%v, reason:%s", err, reason))
 	}
 }
